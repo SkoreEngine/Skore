f5f3c9f8889f6062d59cecd62bf738ede2abf26f
Daethalus <daethalus@gmail.com>
2025-07-18 00:25:36 +0100

    pkpy - tests

--- Runtime/Source/Skore/Scripting/PkPyScriptingEngine.cpp
+++ Runtime/Source/Skore/Scripting/PkPyScriptingEngine.cpp
@@ -28,32 +28,80 @@
 
 namespace Skore
 {
-
 	static Logger& logger = Logger::GetLogger("Skore: PkPyScriptingEngine");
-
 
 	void PkPyScriptResourceChange(ResourceObject& oldValue, ResourceObject& newValue, VoidPtr userData)
 	{
 		PkPyScriptingEngine::Execute(newValue.GetString(PkPyScriptResource::Source), newValue.GetString(PkPyScriptResource::Name));
 	}
 
-
 	void PkPyScriptingEngine::Execute(StringView file, StringView name)
 	{
 		bool ok = py_exec(file.CStr(), name.CStr(), EXEC_MODE, NULL);
 		if (!ok)
 		{
-			char* msg = py_formatexc();
-			logger.Error("Error executing script: {}", msg);
+			logger.Error("Error executing script: {}", py_formatexc());
+			return;
 		}
+
+		logger.Debug("Script {} executed successfully", name);
+	}
+
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	// auto data = py_touserdata(py_getslot(func.ptr(), 0));
+
+	py_Type loggerType;
+
+	// 1. Define a wrapper function with the signature `py_CFunction`.
+	bool py_add(int argc, py_Ref argv)
+	{
+		// 2. Check the number of arguments.
+		PY_CHECK_ARGC(2);
+		// 3. Check the type of arguments.
+		PY_CHECK_ARG_TYPE(0, tp_int);
+		PY_CHECK_ARG_TYPE(1, tp_int);
+		// 4. Convert the arguments into C types.
+		int _0 = py_toint(py_arg(0));
+		int _1 = py_toint(py_arg(1));
+		// 5. Call the original function.
+		int res = add(_0, _1);
+		// 6. Set the return value.
+		py_newint(py_retval(), res);
+		// 7. Return `true`.
+		return true;
+	}
+
+	bool GetLogger(int argc, py_Ref argv)
+	{
+		void* object = py_newobject(py_retval(), loggerType, 1, 0);
+		//object = Logger::GetLogger("Skore::Test");
+		return true;
+	}
+
+
+	bool PrintInfo(int argc, py_Ref argv)
+	{
+		void* userData = py_touserdata(py_arg(0));
+		Logger::GetLogger("Skore::Test").Info("{}", py_tostr(py_arg(1)));
+		py_newnone(py_retval());
+		return true;
 	}
 
 
 	void PkPyScriptingEngineInit()
 	{
 		py_initialize();
-		//py_exec("print('Hello world!')", "<string>", EXEC_MODE, NULL);
 		Resources::FindType<PkPyScriptResource>()->RegisterEvent(PkPyScriptResourceChange, nullptr);
+
+		py_GlobalRef mod = py_newmodule("skore");
+
+		loggerType = py_newtype("Logger", tp_object, mod, nullptr);
+		py_bindstaticmethod(loggerType, "get_logger", GetLogger);
+		py_bindmethod(loggerType, "info", PrintInfo);
+
 
 	}
 
@@ -62,6 +110,4 @@ namespace Skore
 		Resources::FindType<PkPyScriptResource>()->UnregisterEvent(PkPyScriptResourceChange, nullptr);
 		py_finalize();
 	}
-
-
 }
